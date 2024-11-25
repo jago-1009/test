f7e8581c54f6b8342cd187b78ca47292de5a799d
Garwood, Jacob Leroy <jacogarw@iu.edu>
2024-11-25 13:57:13 -0500

    Added Contact Info and Titles/Descriptions.

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ let light = "#ffffff";
 let isDark = true;
 let rotation = 0;
 let isOn = false;
-let projects = ['project1', 'project2', 'project3'];
+const projects = ["project1", "project2", "project3"];
+const projectTitle = ['EPortfolio Administrator Interface', 'Attack of the Front End Web Developer', 'ArmorCoach']
+const projectDesc = ["An Adminstrator Interface for IU Indianapolis' ePortfolio Showcase.", "A short story to learn how to use the Svelte Library along with Sveltekit.", "A website for Historical Fencing academies to assign group orders for their students."]
 function initListeners() {
   $("#scrollBar").css("height", `${$(document.body).height()}px`);
   $("#scrollBackground").css("height", `${$(document.body).height()}px`);
   $("#color").val($(":root").css("--main-color"));
-  $('#grid').hide();
-  
+  $("#grid").hide();
 }
 $(window).scroll(function () {
   const windowHeight = $(window).height();
@@ -18,42 +19,34 @@ $(window).scroll(function () {
   const scrollHeight = $(document).height() - $(window).height();
   const scrollRatio = scrollTop / scrollHeight;
   const windowWidth = $(window).width();
-  const halfWaypoint = (windowWidth / 2);
-  
- 
+  const halfWaypoint = windowWidth / 2;
+
   $(".left").each(function () {
     const boxPosition = $(this).offset().left;
     switch (windowWidth) {
       case windowWidth < 600:
-        if (boxPosition > (windowWidth / 1.5)) {
+        if (boxPosition > windowWidth / 1.5) {
           $(this).hide();
-        }
-        else {
+        } else {
           $(this).show();
         }
         break;
       case windowWidth < 400:
-        if (boxPosition > (windowWidth / 1.4)) {
+        if (boxPosition > windowWidth / 1.4) {
           $(this).hide();
-        }
-        else {
+        } else {
           $(this).show();
         }
         break;
       default:
-        if (boxPosition >= halfWaypoint ) {
+        if (boxPosition >= halfWaypoint) {
           $(this).css("opacity", `0`);
-        }
-        else {
+        } else {
           $(this).css("opacity", `1`);
         }
-      }
-    })
-   
-   
-  
-  
-  
+    }
+  });
+
   const boxHeight = $("#scrollTest").outerHeight();
 
   const thumbPosition = scrollRatio * (windowHeight - boxHeight);
@@ -68,7 +61,7 @@ $(window).resize(function () {
     if (boxPosition > halfWaypoint) {
       $(this).css("opacity", `0`);
     }
-  })
+  });
 });
 
 function getScrollbarThumbPosition(element) {
@@ -131,229 +124,187 @@ $(".colorBtn").on("click", (e) => {
   setColor(color);
   $("#color").val(color);
 });
-$('.lever-body').on("click", function (e) {
+$(".lever-body").on("click", function (e) {
+  const $pageCover = $("#pageCover");
 
-  const $pageCover = $('#pageCover');
+  $("#leverHead").addClass("lever-switch");
+  $("#leverHandle").addClass("lever-switch");
 
-      $('#leverHead').addClass('lever-switch');
-      $('#leverHandle').addClass('lever-switch');
+  setTimeout(() => {
+    $pageCover.removeClass("flicker");
+    $pageCover.addClass("flicker-reverse");
+  }, 1000);
+  setTimeout(() => {
+    $("#lever").css("display", "none");
+  }, 2000);
 
+  window.scrollTo(0, 0);
 
-      setTimeout(() => {
-        $pageCover.removeClass("flicker");
-        $pageCover.addClass("flicker-reverse");
-      }, 1000)
-      setTimeout(() => {
-        $("#lever").css('display', 'none');
-      },2000)
-    
-  
+  setTimeout(() => {
+    $("#steam").addClass("steam-anim");
 
-    window.scrollTo(0, 0)
+    $("#lever").removeClass("fixed");
+    $("#whistle-anim").removeClass("fixed");
+  }, 500);
+  setTimeout(() => {
+    $pageCover.css("animation-play-state", "running");
+    $pageCover.removeClass("flicker-reverse");
+    $pageCover.addClass("flicker");
+  }, 1000);
+  setTimeout(() => {
+    $pageCover.css("display", "none");
+  }, 2000);
+});
+$("#gridBtn").on("click", function (e) {
+  $(".pour-right").animate({ right: "50%" }, 1000);
+  $(".pour-left").animate({ left: "-50%" }, 1000);
+  $(".pour-right").removeAttr("style");
+});
+$("#carouselBtn").on("click", function (e) {  $(".pour-right").animate({ right: "-200px" }, 1000, function () {
+  $(".pour-right").css("right", "200px");
+});
+$(".pour-left").animate({ left: "-290px" }, 1000, function () {
+  $(".pour-left").css("left", "200px");
+});
  
-    setTimeout(() => {
-      
-      $('#steam').addClass('steam-anim')
-     
-      $("#lever").removeClass('fixed')
-      $("#whistle-anim").removeClass('fixed')
-      
-    }, 500)
-    setTimeout(() => {
-      $pageCover.css('animation-play-state', 'running');
-      $pageCover.removeClass("flicker-reverse");
-      $pageCover.addClass("flicker");
-    }, 1000)
-    setTimeout(() => {
-      $pageCover.css('display', 'none');
-    }, 2000)
-  }
-
-)
-// $("#gear").on("click", function (e) {
-//   const $gear = $(this);
-//   rotation += 360;
-//   // Reset the rotation angle to 0 for each click
-
-//   // Rotate the gear
-//   $gear.animate(
-//     { deg: rotation },
-//     {
-//       duration: 1200,
-//       step: function (now) {
-//         $gear.css({ transform: `rotate(${now}deg)` });
-//       },
-//       complete: function () {
-//         if (rotation % 720 == 0) {
-//           $(".pour-right").animate(
-//             { right: "-250px" },
-//             1000,
-            
-//             function () {
-//               $(".pour-right").css( "right", "280px");
-//             }
-//           );
-//           $(".pour-left").animate({ left: "-290px" }, 1000, function () {
-//             $(".pour-left").css( "left", "200px");
-//           });
-//         } else {
-//           $(".pour-right").animate({ right: "40%" }, 1000);
-//           $(".pour-left").animate({ left: "-40%" }, 1000);
-//           $('.pour-right').removeAttr("style");
-//         }
-//       },
-//     }
-//   );
-// });
-$(document).ready(function() {
+});
+$(document).ready(function () {
   $(".animation").each(function () {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          // console.log('is intersecting');
-          $(entry.target).removeClass("run");
-        } else {
-          $(entry.target).addClass("run");
-        }
-      });
-    }, {rootMargin: '200px 0px 0px 0px'});
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            // console.log('is intersecting');
+            $(entry.target).removeClass("run");
+          } else {
+            $(entry.target).addClass("run");
+          }
+        });
+      },
+      { rootMargin: "200px 0px 0px 0px" }
+    );
     observer.observe(this);
   });
 });
 $("#chevron-left").on("click", function (e) {
-  let leftSlideNum ;
-  let rightSlideNum ;
+  let leftSlideNum;
+  let rightSlideNum;
   let leftSlide;
   let rightSlide;
-  var slides = $('.project');
+  var slides = $(".project");
   slides.each(function (index) {
     var slide = $(this);
-    if (slide.attr('id') == 'center') {
-      slide.removeAttr('id');
-      
-      console.log(slide)
-      let currentSlide = $(slide).attr('project-label')
-      let currentIndex = projects.indexOf(currentSlide)
-      
-    
-      if (currentIndex == -1 ) {
-        currentIndex = 2
+    if (slide.attr("id") == "center") {
+      slide.removeAttr("id");
+
+      console.log(slide);
+      let currentSlide = $(slide).attr("project-label");
+      let currentIndex = projects.indexOf(currentSlide);
+
+      if (currentIndex == -1) {
+        currentIndex = 2;
       }
-      console.log(currentIndex)
+      console.log(currentIndex);
       if (currentIndex - 1 < 0) {
-        leftSlideNum = 2; 
-      }
-      else {
-        leftSlideNum = currentIndex - 1
+        leftSlideNum = 2;
+      } else {
+        leftSlideNum = currentIndex - 1;
       }
       if (currentIndex + 1 > 2) {
         rightSlideNum = 0;
+      } else {
+        rightSlideNum = currentIndex + 1;
       }
-      else {
-        rightSlideNum = currentIndex + 1
-      }
-       leftSlide = $(`.${projects[leftSlideNum]}`)
-       rightSlide = $(`.${projects[rightSlideNum]}`)
+      leftSlide = $(`.${projects[leftSlideNum]}`);
+      rightSlide = $(`.${projects[rightSlideNum]}`);
       // console.log(leftSlide)
-      
-      
-      
     }
-  })
-  leftSlide.attr('id', 'center');
+  });
+  leftSlide.attr("id", "center");
   // console.log(rightSlide)
   return initCarousel();
-})
+});
 $("#chevron-right").on("click", function (e) {
-  let leftSlideNum ;
-  let rightSlideNum ;
+  let leftSlideNum;
+  let rightSlideNum;
   let leftSlide;
   let rightSlide;
-  var slides = $('.project');
+  var slides = $(".project");
   slides.each(function (index) {
     var slide = $(this);
-    if (slide.attr('id') == 'center') {
-      slide.removeAttr('id');
-      
-      console.log(slide)
-      let currentSlide = $(slide).attr('project-label')
-      let currentIndex = projects.indexOf(currentSlide)
-      
-    
-      if (currentIndex == -1 ) {
-        currentIndex = 2
+    if (slide.attr("id") == "center") {
+      slide.removeAttr("id");
+
+      console.log(slide);
+      let currentSlide = $(slide).attr("project-label");
+      let currentIndex = projects.indexOf(currentSlide);
+
+      if (currentIndex == -1) {
+        currentIndex = 2;
       }
-      console.log(currentIndex)
+      console.log(currentIndex);
       if (currentIndex - 1 < 0) {
-        leftSlideNum = 2; 
-      }
-      else {
-        leftSlideNum = currentIndex - 1
+        leftSlideNum = 2;
+      } else {
+        leftSlideNum = currentIndex - 1;
       }
       if (currentIndex + 1 > 2) {
         rightSlideNum = 0;
+      } else {
+        rightSlideNum = currentIndex + 1;
       }
-      else {
-        rightSlideNum = currentIndex + 1
-      }
-       leftSlide = $(`.${projects[leftSlideNum]}`)
-       rightSlide = $(`.${projects[rightSlideNum]}`)
+      leftSlide = $(`.${projects[leftSlideNum]}`);
+      rightSlide = $(`.${projects[rightSlideNum]}`);
       // console.log(leftSlide)
-      
-      
-      
     }
-  })
-  rightSlide.attr('id', 'center');
+  });
+  rightSlide.attr("id", "center");
   // console.log(rightSlide)
   return initCarousel();
-})
+});
 function initCarousel() {
-let currentSlide = $('#center');
-currentSlide.removeAttr('style')
-for (let i=0;i<projects.length;i++){  
-  let projNum = projects[i];
-  let slide = $(`.${projNum}`)
-  let isCenter = slide.attr('id')
-  let leftSlide = 0;
-  let rightSlide = 1;
-  if (isCenter) {
-    // console.log(slide)
-    let currentIndex = slide.index() - 1 
-    // console.log(currentIndex)
-    if (currentIndex - 1 < 0) {
-      leftSlide = 2; 
+  let currentSlide = $("#center");
+  currentSlide.removeAttr("style");
+  for (let i = 0; i < projects.length; i++) {
+    let projNum = projects[i];
+    let slide = $(`.${projNum}`);
+    let isCenter = slide.attr("id");
+    let leftSlide = 0;
+    let rightSlide = 1;
+    if (isCenter) {
+      // console.log(slide)
+      let currentIndex = slide.index() - 1;
+      $("#project-title").html(projectTitle[currentIndex])
+      $("#project-desc").html(projectDesc[currentIndex])
+      if (currentIndex - 1 < 0) {
+        leftSlide = 2;
+      } else {
+        leftSlide = currentIndex - 1;
+      }
+      if (currentIndex + 1 > 2) {
+        rightSlide = 0;
+      } else {
+        rightSlide = currentIndex + 1;
+      }
+      let left = $(`.${projects[leftSlide]}`);
+      let right = $(`.${projects[rightSlide]}`);
+      left.css("transform", "translateX(-30%) scale(0.9)");
+      left.css("opacity", "0.5");
+      right.css("transform", "translateX(30%) scale(0.9)");
+      right.css("opacity", "0.5");
     }
-    else {
-      leftSlide = currentIndex - 1
-    }
-    if (currentIndex + 1 > 2) {
-      rightSlide = 0;
-    }
-    else {
-      rightSlide = currentIndex + 1
-    }
-    let left = $(`.${projects[leftSlide]}`)
-    let right = $(`.${projects[rightSlide]}`)
-    left.css('transform', 'translateX(-30%) scale(0.9)')
-    left.css('opacity', '0.5')
-    right.css('transform', 'translateX(30%) scale(0.9)')
-    right.css('opacity', '0.5')
   }
-
 }
-}
-$("#gridBtn").on('click', function() {
-  $('#carousel').hide()
-  $("#grid").show()
-}) 
-$("#carouselBtn").on('click', function() {
-  $('#grid').hide()
-  $("#carousel").show()
-}) 
-
+$("#gridBtn").on("click", function () {
+  $("#carousel").hide();
+  $("#grid").show();
+});
+$("#carouselBtn").on("click", function () {
+  $("#grid").hide();
+  $("#carousel").show();
+});
 
 $(document).ready(function () {
   initListeners();
   initCarousel();
-})
+});
